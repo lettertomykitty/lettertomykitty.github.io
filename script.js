67d93e0e9eecbe94a9fceb3c26371f90057cb291
lettertomykitty <careov@126.com>
2025-04-10 22:17:07 +0800

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ noButton.addEventListener("click", function () {
   // 图片变化（前 5 次变化）
   if (clickCount === 1) mainImage.src = "images/震惊.png"; // 震惊
   if (clickCount === 2) mainImage.src = "images/思考.png"; // 思考
-  if (clickCount === 3) mainImage.src = "images/生气.png"; // 生气
+  if (clickCount === 3) mainImage.src = "images/愤怒.png"; // 生气
   if (clickCount === 4) mainImage.src = "images/哭泣.png"; // 哭
   if (clickCount >= 5) mainImage.src = "images/哭泣.png"; // 之后一直是哭
 });
